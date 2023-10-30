42da1fb3e3c7bac2ee9ca653025f1ca8804e461e
CryptoSnowPrince <topdirector2017@gmail.com>
2023-10-30 18:05:29 +0900

    smart contract re-init

--- smart-contracts/test/Lock.js
+++ smart-contracts/test/Lock.js
@@ -1,10 +1,9 @@
-import {
+const {
   time,
   loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
 
 describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -44,7 +43,7 @@ describe("Lock", function () {
         deployOneYearLockFixture
       );
 
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
+      expect(await ethers.provider.getBalance(lock.address)).to.equal(
         lockedAmount
       );
     });
