59cc0dfc0809dc0c675fff894d3986fd55ee40ea
CryptoSnowPrince <topdirector2017@gmail.com>
2023-11-02 21:57:40 +0900

    update wallet connect button

--- frontend/src/components/Connect.js
+++ frontend/src/components/Connect.js
@@ -6,7 +6,7 @@ export const Connect = function (props) {
   return (
     <React.Fragment>
       <div
-        className="font-semibold uppercase lg:text-2xl text-xl lg:border-4 border-2 border-trumpos-panelLight rounded-full hover:text-trumpos-fontLight text-center border-yellow-300 px-2 mx-auto lg:mx-0 my-auto py-2 rounded-full text-yellow-300 items-center"
+        className="font-semibold uppercase lg:text-2xl text-xl hover:text-trumpos-fontLight text-center border-yellow-300 px-2 mx-auto lg:mx-0 my-auto py-2 bg-gray-200/[0.3] hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 text-yellow-300 items-center"
       >
         <ConnectWallet />
       </div>
