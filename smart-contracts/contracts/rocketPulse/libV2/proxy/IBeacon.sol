c34de267ac34e23029c235fd349a650d26b344f5
CryptoSnowPrince <topdirector2017@gmail.com>
2023-11-04 13:51:59 +0100

    split contracts

---
+++ smart-contracts/contracts/rocketPulse/libV2/proxy/IBeacon.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.8.20;
+
+// SPDX-License-Identifier: MIT
+
+// File contracts/openzepplin-contracts/proxy/beacon/IBeacon.sol
+
+
+// OpenZeppelin Contracts v4.4.1 (proxy/beacon/IBeacon.sol)
+
+
+/**
+ * @dev This is the interface that {BeaconProxy} expects of its beacon.
+ */
+interface IBeacon {
+    /**
+     * @dev Must return an address that can be used as a delegate call target.
+     *
+     * {BeaconProxy} will check that this address is a contract.
+     */
+    function implementation() external view returns (address);
+}
